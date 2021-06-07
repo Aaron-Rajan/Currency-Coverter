a78a1d02238c315768920d8b711fa2cd86ceb660
Aaron <aaronmraj@gmail.com>
2021-06-06 21:44:52 -0400

    finished creating curr_list using JSON

--- GUI.java
+++ GUI.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class GUI {
+    public static ArrayList<String> arrayList = new ArrayList<>();
     public GUI() {
         JFrame frame = new JFrame();
         frame.setSize(500, 400);
@@ -21,7 +30,7 @@ public class GUI {
         JPanel main_panel = new JPanel();
         main_panel.setPreferredSize(new Dimension(100, 300));
         main_panel.setLayout(new FlowLayout(FlowLayout.CENTER, 75, 10));
-        main_panel.setBackground(Color.darkGray);
+        main_panel.setBackground(Color.lightGray);
         JLabel input_currency_label = new JLabel("From:");
         input_currency_label.setForeground(Color.black);
         input_currency_label.setFont(new Font("", Font.PLAIN, 16));
@@ -30,11 +39,39 @@ public class GUI {
         output_currency_label.setFont(new Font("", Font.PLAIN, 16));
         main_panel.add(input_currency_label);
         main_panel.add(output_currency_label);
+
+        JSONParser jsonP = new JSONParser();
+        try (FileReader fr = new FileReader("currencies.json")) {
+            Object obj = jsonP.parse(fr);
+            JSONArray empList = (JSONArray) obj;
+            empList.forEach(emp -> parseEmpObj((JSONObject) emp));
+
+            String[] curr_list = new String[118];
+            for (int i=0; i<118; i++) {
+                curr_list[i] = arrayList.get(i);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
+
+
+
         frame.add(main_panel);
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+    public static void parseEmpObj(JSONObject emp) {
+        String currency = (String) emp.get("code");
+        arrayList.add(currency);
     }
 
     public static void main(String[] args) {
